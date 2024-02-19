4ca3fd2454ad05ce69cc0f166de41a43fcc133e8
jenniferchristine <jeja2306@student.miun.se>
2024-02-19 14:55:54 +0100

    inserting data to both charts

--- src/js/fetch.js
+++ src/js/fetch.js
@@ -6,13 +6,17 @@ window.onload = async () => {
         const response = await fetch(url);
 
         if (response.ok) {
-
             const data = await response.json();
+
             const coursesHT23 = data.filter(course => course.admissionRound === "HT2023");
             coursesHT23.sort((a, b) => parseInt(b.applicantsTotal) - parseInt(a.applicantsTotal));
-            const top6Courses = coursesHT23.slice(0, 6);
 
-            updateChartWithData(top6Courses);
+            const top5Courses = coursesHT23.slice(0, 5);
+            const programNames = top5Courses.map(course => course.name);
+            const applicantsTotal = top5Courses.map(course => parseInt(course.applicantsTotal));
+
+            updateBarChartWithData(programNames, applicantsTotal);
+            updateRoundChartWithData(programNames, applicantsTotal);
 
         } else {
             console.error("ERROR: " + response.statusText);
@@ -22,18 +26,15 @@ window.onload = async () => {
     }
 }
 
-function updateChartWithData(data) {
-    const courseNames = data.map(course => course.name);
-    const applicantsTotal = data.map(course => parseInt(course.applicantsTotal));
-
-    const ctx = document.getElementById('barChart').getContext('2d');
+function updateBarChartWithData(data, totalApplicants) {
+    const ctx = document.getElementById('barChart');
     const barChart = new Chart(ctx, {
         type: 'bar',
         data: {
-            labels: courseNames,
+            labels: data,
             datasets: [{
                 label: 'Antal Sökande',
-                data: applicantsTotal,
+                data: totalApplicants,
                 backgroundColor: 'rgba(35, 87, 161)',
                 borderColor: 'rgba(54, 162, 235, 1)',
                 borderWidth: 1
@@ -49,37 +50,38 @@ function updateChartWithData(data) {
     });
 }
 
-var ctx = document.getElementById('doughnutChart').getContext('2d');
-var myChart = new Chart(ctx, {
-    type: 'doughnut',
-    data: {
-        labels: ['#1', '#2', '#3', '#4', '#5'],
-        datasets: [{
-            label: '# of Votes',
-            data: [20, 20, 20, 20, 20],
-
-            backgroundColor: [
-                'rgb(35, 87, 161)',
-                'rgb(174, 213, 164)',
-                'rgb(97, 181, 250)',
-                'rgb(25, 144, 168)',
-                'rgb(32, 185, 216)'
-            ],
-            borderColor: [
-                'rgb(29, 75, 141)',
-                'rgb(120, 146, 113)',
-                'rgb(75, 142, 196)',
-                'rgb(19, 107, 125)',
-                'rgb(23, 138, 161)'
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            y: {
-                beginAtZero: true
+function updateRoundChartWithData(data, totalApplicants) {
+    const ctx = document.getElementById('doughnutChart');
+    const doughnutChart = new Chart(ctx, {
+        type: 'doughnut',
+        data: {
+            labels: data,
+            datasets: [{
+                label: 'Antal Sökande',
+                data: totalApplicants,
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.7)',
+                    'rgba(54, 162, 235, 0.7)',
+                    'rgba(255, 206, 86, 0.7)',
+                    'rgba(75, 192, 192, 0.7)',
+                    'rgba(153, 102, 255, 0.7)'
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
             }
         }
-    }
-});
+    });
+}
