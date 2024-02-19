24d01a86b474f52ca5fd1599e1acf9b821e2aea1
jenniferchristine <jeja2306@student.miun.se>
2024-02-19 13:55:05 +0100

    fetched data for one chart and created another

--- src/js/fetch.js
+++ src/js/fetch.js
@@ -1,31 +1,53 @@
 "use strict";
 
-var ctx = document.getElementById('barChart').getContext('2d');
-var myChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: ['#1', '#2', '#3', '#4', '#5', '#6'],
-        datasets: [{
-            label: 'Dataset 1',
-            data: [12, 19, 3, 5, 2, 3, 10],
-            backgroundColor: 'rgb(97, 181, 250)',
-            borderColor: 'rgb(75, 142, 196)',
+window.onload = async () => {
+    const url = "https://studenter.miun.se/~mallar/dt211g/";
+    try {
+        const response = await fetch(url);
+
+        if (response.ok) {
+
+            const data = await response.json();
+            const coursesHT23 = data.filter(course => course.admissionRound === "HT2023");
+            coursesHT23.sort((a, b) => parseInt(b.applicantsTotal) - parseInt(a.applicantsTotal));
+            const top6Courses = coursesHT23.slice(0, 6);
+
+            updateChartWithData(top6Courses);
+
+        } else {
+            console.error("ERROR: " + response.statusText);
+        }
+    } catch (error) {
+        console.error("Issue regarding fetch operation:", error);
+    }
+}
+
+function updateChartWithData(data) {
+    const courseNames = data.map(course => course.name);
+    const applicantsTotal = data.map(course => parseInt(course.applicantsTotal));
+
+    const ctx = document.getElementById('barChart').getContext('2d');
+    const barChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: courseNames,
+            datasets: [{
+                label: 'Antal Sökande',
+                data: applicantsTotal,
+                backgroundColor: 'rgba(35, 87, 161)',
+                borderColor: 'rgba(54, 162, 235, 1)',
+                borderWidth: 1
+            }]
         },
-        {
-            label: 'Dataset 2',
-            data: [15, 8, 25, 7, 14, 10, 5],
-            backgroundColor: 'rgb(174, 213, 164)',
-            borderColor: 'rgb(120, 146, 113)',
-        }]
-    },
-    options: {
-        scales: {
-            y: {
-                beginAtZero: true
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
             }
         }
-    }
-});
+    });
+}
 
 var ctx = document.getElementById('doughnutChart').getContext('2d');
 var myChart = new Chart(ctx, {
