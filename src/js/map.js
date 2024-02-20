d68d0db17d46b5f4da9f1cc4e548060fe6a782b6
jenniferchristine <jeja2306@student.miun.se>
2024-02-20 21:38:13 +0100

    added search button and function to it

--- src/js/map.js
+++ src/js/map.js
@@ -10,27 +10,36 @@ window.onload = async () => {
     const marker = L.marker([0, 0]).addTo(map);
 
     const searchInput = document.getElementById('searchInput');
-    searchInput.addEventListener('keypress', async function (e) {
-        if (e.key === 'Enter') {
-            const searchQuery = searchInput.value;
-            const url = `https://nominatim.openstreetmap.org/search?format=json&q=${searchQuery}`;
-            try {
-                const response = await fetch(url);
-                if (response.ok) {
-                    const data = await response.json();
-                    if (data.length > 0) {
-                        const { lat, lon } = data[0];
-                        map.setView([lat, lon], 13);
-                        marker.setLatLng([lat, lon]);
-                    } else {
-                        console.error("Platsen hittades inte.");
-                    }
-                } else {
-                    console.error("Något gick fel med API-anropet:", response.statusText);
-                }
-            } catch (error) {
-                console.error("Ett fel uppstod:", error);
-            }
-        }
+    const searchButton = document.getElementById('searchBtn');
+
+    searchButton.addEventListener('click', async function (event) {
+        await toSearch();
     });
+
+    searchInput.addEventListener('keypress', async function (e) {
+        if (e.key === 'Enter')
+            await toSearch();
+    });
+
+    async function toSearch() {
+        const searchQuery = searchInput.value;
+        const url = `https://nominatim.openstreetmap.org/search?format=json&q=${searchQuery}`;
+        try {
+            const response = await fetch(url);
+            if (response.ok) {
+                const data = await response.json();
+                if (data.length > 0) {
+                    const { lat, lon } = data[0];
+                    map.setView([lat, lon], 13);
+                    marker.setLatLng([lat, lon]);
+                } else {
+                    console.error("Platsen hittades inte.");
+                }
+            } else {
+                console.error("Något gick fel med API-anropet:", response.statusText);
+            }
+        } catch (error) {
+            console.error("Ett fel uppstod:", error);
+        }
+    }
 }
